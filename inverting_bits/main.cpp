3d5ebf12d57ca587f1ee73d6502841751ce7b307
TaisaKiarkhi <tayisiyakiarkhi@gmail.com>
2023-05-28 17:22:32 +0200

    Update main.cpp

--- inverting_bits/main.cpp
+++ inverting_bits/main.cpp
@@ -15,7 +15,7 @@ int main() {
 		      << "  Inverted value plus 1: " << std::bitset<32>(inverted_number + 1)
 	          << std::bitset<32>(inverted_number) << "   Unsigned inverted value: " << std::bitset<32>(u_inverted_number);
 
-	std::cout<<std::endl << "Integer  representation: " << std::endl;
+	std::cout<<std::endl << "\nInteger  representation: " << std::endl;
 	std::cout << "Original value: " << number << "  Inverted value: " << inverted_number << "   Inverted value plus 1: " << inverted_number + 1
 		      << "  Unsigned inverted value: " << u_inverted_number << std::endl;
 
