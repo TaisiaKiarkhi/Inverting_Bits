0f42d8f9bbfc2197916c378ef3e63e26c48c8422
TaisaKiarkhi <tayisiyakiarkhi@gmail.com>
2023-05-28 17:19:06 +0200

    Update main.cpp

--- inverting_bits/main.cpp
+++ inverting_bits/main.cpp
@@ -11,11 +11,12 @@ int main() {
 	unsigned int u_inverted_number = static_cast<unsigned int>(inverted_number);
 
 	std::cout << "Bit representation: " << std::endl;
-	std::cout << "Original value: " << std::bitset<32>(number) << "  Inverted value: "
+	std::cout << "Original value: " << std::bitset<32>(number) << "  Inverted value: "<< std::bitset<32>(inverted_number)
+		      << "  Inverted value plus 1: " << std::bitset<32>(inverted_number + 1)
 	          << std::bitset<32>(inverted_number) << "   Unsigned inverted value: " << std::bitset<32>(u_inverted_number);
 
 	std::cout<<std::endl << "Integer  representation: " << std::endl;
-	std::cout << "Original value: " << number << "  Inverted value: " << inverted_number 
+	std::cout << "Original value: " << number << "  Inverted value: " << inverted_number << "   Inverted value plus 1: " << inverted_number + 1
 		      << "  Unsigned inverted value: " << u_inverted_number << std::endl;
 
 
